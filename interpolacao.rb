8bcf8641ace3e71d8e5144f7168557e1298ebda6
wacoders <w.silva@qacoders-treinamento.com>
2022-05-03 21:21:04 -0300

    operadores logico

---
+++ interpolacao.rb
@@ -0,0 +1,7 @@
+nome = "William"
+sobreNome = "Alves da Silva"
+
+puts nome + sobreNome
+puts nome + " " + sobreNome
+puts "#{nome} #{sobreNome}"
+puts "sfdfsdfsd sdfsdfsdf"
---
+++ operadores_logico_unless.rb
@@ -0,0 +1,4 @@
+x = gets.to_i 
+unless x <= 3
+    puts "Falso"
+end
